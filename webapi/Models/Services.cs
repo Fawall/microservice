3c654e1dbaba6b0912aea0cb5a1cdf43ac5bb47f
fabio lopes <fabiolopes7844@gmail.com>
2020-10-25 15:17:19 -0300

    Create ServiceModel

---
+++ webapi/Models/Services.cs
@@ -0,0 +1,11 @@
+namespace webapi.Models
+{
+    public class Services
+    {
+        public string City { get; set;}
+        public int Temp {get; set;}
+        public string Music {get; set;}
+
+
+    }
+}
