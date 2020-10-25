0abf6132644e0161b569d5998d9914f8f0d2bf8e
fabio lopes <fabiolopes7844@gmail.com>
2020-10-25 15:07:03 -0300

    name class Desearialize renamed for Deserialize

---
+++ webapi/Helpers/Deserialize.cs
@@ -0,0 +1,10 @@
+using System.Threading.Tasks;
+
+namespace webapi.Controllers
+{
+    public class Deserialize 
+    {
+        public int Temp { get; set;}
+        public string city {get; set;}
+    }
+}
