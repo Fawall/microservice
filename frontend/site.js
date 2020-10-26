259c625ef4cf40640df17252d3b2b18c0a6e43a1
fabio lopes <fabiolopes7844@gmail.com>
2020-10-25 21:13:08 -0300

    removed console.log in function Cidades

--- frontend/site.js
+++ frontend/site.js
@@ -2,11 +2,8 @@ function Cidades(){
     let select = document.getElementById('testes');
     let value = select[select.selectedIndex].value
     
-    console.log(value);
-
     return value
 }
-
 
 let DivCity = document.getElementById("city")
 let DivTemp = document.getElementById("temp")
@@ -15,8 +12,7 @@ let DivMusic = document.getElementById("music")
 function GetUrl(){
 
     let uri = `http://localhost:5000/api/temperature/${Cidades()}`
-    
-    
+      
     fetch(uri, {method: "GET"}).then(function(response){
         
         return response.text().then(function(text){
