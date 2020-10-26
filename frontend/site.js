742cd2aecfd4ee07984a267f9e1c085a2ce49494
fabio lopes <fabiolopes7844@gmail.com>
2020-10-25 21:18:34 -0300

    renamed name function Cidades from Cities

--- frontend/site.js
+++ frontend/site.js
@@ -1,26 +1,26 @@
-function Cidades(){
-    let select = document.getElementById('testes');
-    let value = select[select.selectedIndex].value
+function Cities(){
+    let select = document.getElementById('capital');
+    let value = select[select.selectedIndex].value;
     
-    return value
+    return value;
 }
 
-let DivCity = document.getElementById("city")
-let DivTemp = document.getElementById("temp")
-let DivMusic = document.getElementById("music")
+let DivCity = document.getElementById("city");
+let DivTemp = document.getElementById("temp");
+let DivMusic = document.getElementById("music");
 
 function GetUrl(){
 
-    let uri = `http://localhost:5000/api/temperature/${Cidades()}`
+    let uri = `http://localhost:5000/api/temperature/${Cities()}`
       
     fetch(uri, {method: "GET"}).then(function(response){
         
         return response.text().then(function(text){
             const cidadeInfo = JSON.parse(text)
 
-            DivCity.innerHTML = cidadeInfo.city
-            DivTemp.innerHTML = cidadeInfo.temp
-            DivMusic.innerHTML = cidadeInfo.music
+            DivCity.innerHTML = cidadeInfo.city;
+            DivTemp.innerHTML = cidadeInfo.temp;
+            DivMusic.innerHTML = cidadeInfo.music;
         });
     });
                // async x => await x.text()
